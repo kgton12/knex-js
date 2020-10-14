b638554da8134d7d56a1eabbf6031a29abb5d71c
Welington Oliveira <56723945+kgton12@users.noreply.github.com>
2020-10-14 10:06:44 -0300

    Estudo Knex

--- src/server.js
+++ src/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const knex = require('./database');
+const routes = require('./routes');
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (req, res) =>
-  knex('users').then((results) =>
-    res.json(results)))
+app.use(routes);
+//notFound
+app.use((req, res, next)=>{
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
+//Catch all
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: error.message
+  })
+})
 
 app.listen(3333, () => console.log('Server is running'));
