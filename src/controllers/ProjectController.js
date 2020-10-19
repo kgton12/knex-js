6711dae580a17c221701ba4891143b2f4c80e6d5
Welington Oliveira <56723945+kgton12@users.noreply.github.com>
2020-10-19 08:24:33 -0300

    knex

---
+++ src/controllers/ProjectController.js
@@ -0,0 +1,34 @@
+const knex = require('../database');
+module.exports = {
+  async index(req, res, next) {
+    try {
+      const { user_id } = req.query
+
+      const query = knex('projects')
+
+      if (user_id) {
+        query
+          .where({ user_id })
+          .join('users', 'users.id', '=', 'projects.user_id')
+          .select('projects.*', 'users.username')
+      }
+      const results = await query
+
+      return res.json(results);
+    } catch (error) {
+      next(error)
+    }
+  },
+  async create(req, res, next) {
+    try {
+      const { title, user_id } = req.body;
+      await knex('projects').insert({
+        title,
+        user_id
+      })
+      return res.status(201).send()
+    } catch (error) {
+      next(error)
+    }
+  }
+}
